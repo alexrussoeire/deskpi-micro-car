9af0d8adb2bda74b620c01891a17b3731a9ba6e4
alexrussoeire <alexrussoeire@gmail.com>
2026-01-03 21:19:11 +0000

    simplified the namespaces

--- motor-module/main.ts
+++ motor-module/main.ts
@@ -2,13 +2,13 @@
  * Custom blocks for the Deskpi Motor Extension
  */
 //% weight=100 color=#0fbc11 icon="\uf11b" block="motor"
-namespace deskpi_motor_module {
+namespace motor {
 
     /**
-     * Drives the robot forward at a specific speed.
+     * Drives the car forward at a specific speed.
      * @param speed the speed of the motor, eg: 50
      */
-    //% blockId=deskpi_motor_drive_forward 
+    //% blockId=motor_drive_forward 
     //% block="drive forward at speed %speed"
     //% speed.min=0 speed.max=100
     export function driveForward(speed: number): void {
@@ -17,10 +17,10 @@ namespace deskpi_motor_module {
     }
 
     /**
-     * Stops the robot immediately.
+     * Stops the car immediately.
      */
-    //% blockId=deskpi_motor_stop 
-    //% block="stop the robot"
+    //% blockId=motor_stop 
+    //% block="stop the car"
     //% weight=90
     export function stop(): void {
         // Logic to cut power to motors
--- ultrasonic-module/main.ts
+++ ultrasonic-module/main.ts
@@ -1,15 +1,15 @@
 /**
- * Custom blocks for the Deskpi Ultrasonic Extension
+ * Custom blocks for the Deskpi Sonar Extension
  */
-//% weight=100 color=#0fbc11 icon="\uf11b" block="ultrasonic"
-namespace deskpi_ultrasonic_module {
+//% weight=100 color=#0fbc11 icon="\uf11b" block="sonar"
+namespace sonar {
 
     /**
      * Gets the sonar distance.
      */
     //% blockId=robot_get_sonar_distance 
     //% block="get sonar distance"
-    export function get_sonar_distance(): number {
+    export function get_distance(): number {
         // Your actual hardware logic goes here
         //basic.showArrow(ArrowNames.North);
         return 42;
